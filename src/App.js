0300885b00e1015ce689c4389b9d5c161a558e9f
Jair Ramirez <jairramirez@MacBook-Air-de-Jair.local>
2023-04-18 22:03:20 -0600

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import Header from './componentes/Header';
+import FormularioTareas from './componentes/FormularioTareas';
+import ListaTareas from './componentes/ListaTareas';
+
+const App = () => {
+
+  const tareasGuardadas = 
+    localStorage.getItem('tareas') ? 
+    JSON.parse(localStorage.getItem('tareas')) : []; 
+
+  const [tareas, cambiarTareas] = useState(tareasGuardadas);
+   
+  useEffect(() => {
+    localStorage.setItem('tareas', JSON.stringify(tareas));
+  }, [tareas]);
+
+  let configMostrarCompletadas = '';
+  if(localStorage.getItem('mostrarCompletadas') === null){
+    configMostrarCompletadas = true;
+  }else {
+   configMostrarCompletadas = localStorage.getItem('mostrarCompletadas') === 'true';
+  }
+
+  const [mostrarCompletadas, cambiarMostrarCompletadas] = useState(configMostrarCompletadas);
+
+  useEffect(() => {
+    localStorage.setItem('mostrarCompletadas', mostrarCompletadas.toString());
+  }, [mostrarCompletadas]);
+
+  return (
+    <div className="contenedor">
+        <Header 
+          mostrarCompletadas={mostrarCompletadas} 
+          cambiarMostrarCompletadas={cambiarMostrarCompletadas}
+        />
+        <FormularioTareas tareas={tareas} cambiarTareas={cambiarTareas}/>
+        <ListaTareas 
+          tareas={tareas} 
+          cambiarTareas={cambiarTareas}
+          mostrarCompletadas={mostrarCompletadas}
+         />
+    </div>
+  );
+}
+
+export default App;
