56139947579bdd236a48f31e29d00b04dab5b40b
Jair Ramirez <jairramirez@MacBook-Air-de-Jair.local>
2023-04-29 08:11:17 -0600

    Preparacion de Rutas LocalStorga de API

--- src/App.js
+++ src/App.js
@@ -11,59 +11,22 @@ import './index.css';
 import { useParams } from "react-router-dom";
 import { Link } from 'react-router-dom';
 import New from "./componentes/New";
+import { NavLink } from 'react-router-dom';
 
 const App = () => {
 
-  const tareasGuardadas = 
-    localStorage.getItem('tareas') ? 
-    JSON.parse(localStorage.getItem('tareas')) : []; 
-
-  const [tareas, cambiarTareas] = useState(tareasGuardadas);
-   
-  useEffect(() => {
-    
-    localStorage.setItem('tareas', JSON.stringify(tareas));
-  }, [tareas]);
-/*
-  useEffect(async () =>{
-    const tareasAPI = await getTask();
-    console.log(tareasAPI)
-  },[]);
-*/
-  let configMostrarCompletadas = '';
-  if(localStorage.getItem('mostrarCompletadas') === null){
-    configMostrarCompletadas = true;
-  }else {
-   configMostrarCompletadas = localStorage.getItem('mostrarCompletadas') === 'true';
-  }
-
-  const [mostrarCompletadas, cambiarMostrarCompletadas] = useState(configMostrarCompletadas);
-
-  useEffect(() => {
-    localStorage.setItem('mostrarCompletadas', mostrarCompletadas.toString());
-  }, [mostrarCompletadas]);
-
-  
-  const handleClick = () => {
-    window.location.replace('http://localhost:3000/NEW');
-  };
 
   return (
     <div className="contenedor">
-        <Header 
-          mostrarCompletadas={mostrarCompletadas} 
-          cambiarMostrarCompletadas={cambiarMostrarCompletadas}
-        />  
-        <FormularioTareas tareas={tareas} cambiarTareas={cambiarTareas}/>
-        <ListaTareas 
-          tareas={tareas} 
-          cambiarTareas={cambiarTareas}
-          mostrarCompletadas={mostrarCompletadas}
-         />
+         <nav>
+            <a href='/'>Tasks</a>
+            <a href='/api'>API</a>
+         </nav>
          
          <BrowserRouter>
           <Routes>
-            <Route path='/' element={<Inicio></Inicio>}></Route>
+            <Route path='/api' element={<Inicio></Inicio>}></Route>
+            <Route path='/' element={<New/>}> </Route>
             <Route path='/personaje/:id' element={<Personaje></Personaje>}></Route>
           </Routes>
          </BrowserRouter>
--- src/componentes/New.js
+++ src/componentes/New.js
@@ -1,13 +1,58 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import Header from './Header'
+import FormularioTareas from './FormularioTareas';
+import ListaTareas from './ListaTareas';
 import Inicio from './Inicio'
 import Personaje from './Personaje'
+import {useState, useEffect} from 'react'
 
 function New() {
+  const tareasGuardadas = 
+  localStorage.getItem('tareas') ? 
+  JSON.parse(localStorage.getItem('tareas')) : []; 
+
+const [tareas, cambiarTareas] = useState(tareasGuardadas);
+ 
+useEffect(() => {
+  
+  localStorage.setItem('tareas', JSON.stringify(tareas));
+}, [tareas]);
+/*
+useEffect(async () =>{
+  const tareasAPI = await getTask();
+  console.log(tareasAPI)
+},[]);
+*/
+let configMostrarCompletadas = '';
+if(localStorage.getItem('mostrarCompletadas') === null){
+  configMostrarCompletadas = true;
+}else {
+ configMostrarCompletadas = localStorage.getItem('mostrarCompletadas') === 'true';
+}
+
+const [mostrarCompletadas, cambiarMostrarCompletadas] = useState(configMostrarCompletadas);
+
+useEffect(() => {
+  localStorage.setItem('mostrarCompletadas', mostrarCompletadas.toString());
+}, [mostrarCompletadas]);
+
+
+const handleClick = () => {
+  window.location.replace('http://localhost:3000/NEW');
+};
   return (
     <div>
-         <h1>kjflk</h1>
+         <Header 
+          mostrarCompletadas={mostrarCompletadas} 
+          cambiarMostrarCompletadas={cambiarMostrarCompletadas}
+        />  
+        <FormularioTareas tareas={tareas} cambiarTareas={cambiarTareas}/>
+        <ListaTareas 
+          tareas={tareas} 
+          cambiarTareas={cambiarTareas}
+          mostrarCompletadas={mostrarCompletadas}
+         />
          
     </div>
   );
