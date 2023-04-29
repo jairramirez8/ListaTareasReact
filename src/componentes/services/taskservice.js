a7306887afd6a622cb3775ea5e9031cdfb691cd1
Jair Ramirez <jairramirez@MacBook-Air-de-Jair.local>
2023-04-29 08:35:10 -0600

    Simplificacion de clases

--- src/componentes/services/taskservice.js
+++ src/componentes/services/taskservice.js
@@ -1,12 +1,5 @@
 import axios from "axios";
 
-const APIURL = new URL("https://taskapi.juanfrausto.com/api/task");
-
-export const getTask = async ()=>{
-    const response = await axios.get(APIURL)
-    return response.data
-}
-
 const allcaracters = async (state) => {
     const peticion = await axios.get('https://rickandmortyapi.com/api/character')
     state(peticion.data.results)
